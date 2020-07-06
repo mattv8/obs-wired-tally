89c11b8e045c943f65f71d950cf0e555453a8e22
Matt Visnovsky <matt@visnovsky.us>
2020-07-06 14:14:42 -0600

    Added rainbow effect
    Rainbow effect when serial is not available.

--- Wired (Arduino)/Compiled/toArduino/OBS-tallyWithPrev/OBS-tallyWithPrev.ino
+++ Wired (Arduino)/Compiled/toArduino/OBS-tallyWithPrev/OBS-tallyWithPrev.ino
@@ -4,9 +4,14 @@
 //
 //  Written by: Matthew P. Visnovsky
 //  Adapted from https://boprograms.eu/obsTally/.
-//
+//  Includes code by Mark Kriegsman.
 //
 #include "FastLED.h" //Using FastLED Library https://github.com/FastLED/FastLED
+
+// Dependency check
+#if FASTLED_VERSION < 3001000
+#error "Requires FastLED 3.1 or later; check github for latest code."
+#endif
 
 // -----------------------PINS------------------------- //
 #define Vout     2   // Digital
@@ -46,6 +51,48 @@ int lastSerialState = 0;
 
 // ------------------------FUNCTIONS------------------------ //
 
+// This function draws rainbows with an ever-changing, widely-varying set of parameters. 
+// Function credit goes to Mark Kriegsman (2015).
+void rainbow() 
+{
+  static uint16_t sPseudotime = 0;
+  static uint16_t sLastMillis = 0;
+  static uint16_t sHue16 = 0;
+ 
+  uint8_t sat8 = beatsin88( 87, 220, 250);
+  uint8_t brightdepth = beatsin88( 341, 96, 224);
+  uint16_t brightnessthetainc16 = beatsin88( 203, (25 * 256), (40 * 256));
+  uint8_t msmultiplier = beatsin88(147, 23, 60);
+
+  uint16_t hue16 = sHue16;//gHue * 256;
+  uint16_t hueinc16 = beatsin88(113, 1, 3000);
+  
+  uint16_t ms = millis();
+  uint16_t deltams = ms - sLastMillis ;
+  sLastMillis  = ms;
+  sPseudotime += deltams * msmultiplier;
+  sHue16 += deltams * beatsin88( 400, 5,9);
+  uint16_t brightnesstheta16 = sPseudotime;
+  
+  for( uint16_t i = 0 ; i < NUM_LEDS*NUM_SETS; i++) {
+    hue16 += hueinc16;
+    uint8_t hue8 = hue16 / 256;
+
+    brightnesstheta16  += brightnessthetainc16;
+    uint16_t b16 = sin16( brightnesstheta16  ) + 32768;
+
+    uint16_t bri16 = (uint32_t)((uint32_t)b16 * (uint32_t)b16) / 65536;
+    uint8_t bri8 = (uint32_t)(((uint32_t)bri16) * brightdepth) / 65536;
+    bri8 += (255 - brightdepth);
+    
+    CRGB newcolor = CHSV( hue8, sat8, bri8);
+    
+    uint16_t pixelnumber = i;
+    pixelnumber = (NUM_LEDS*NUM_SETS-1) - pixelnumber;
+    
+    nblend( leds[pixelnumber], newcolor, 64);
+  }
+}
 
 // ------------------------SETUP LOOP------------------------------- //
 void setup() {
@@ -117,8 +164,8 @@ void loop(void) {
 
   }else { //If not recieving any data
     
-    FastLED.setBrightness(DUTY_CYCLE*.02);//2% duty cycle for all lights at once
-    fill_solid( leds, TOTAL_LEDS, CRGB::White ); //Fill all LEDs gray
+    FastLED.setBrightness(DUTY_CYCLE*.05);//5% duty cycle for all lights at once
+    rainbow(); //Fill all LEDs with beautiful dancing rainbows while we wait for Serial
     
   }//End Serial.available()
 
